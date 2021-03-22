f85073bfdce6820d6431c52cca04d2110334034b
work-development <52549151+work-development@users.noreply.github.com>
2021-03-23 00:22:39 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -5,16 +5,13 @@ from django.views.generic import CreateView
 from .forms import CreationForm
 from posts.models import  User, Post
 
-#  Здесь вроде два пробела должно быть?
+
 class SignUp(CreateView):
         form_class = CreationForm
         success_url = "/auth/login/"
         template_name = "signup.html"
 
-# "...А коммитить код без них" Вы имеете ввиду сохранить у себя на компе две версии 
-# проекта и в одной (для себя) все комментить а вторую (пустую, без комментов) отправлять на праверку?
 
-# Сейчас оставил только важные комментарии
 
 
 
